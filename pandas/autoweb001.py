51727191bfe45b5db1faa72f6db871fe31d941e8
agungsriyono <96234293+agungsriyono@users.noreply.github.com>
2022-06-26 23:36:54 +0700

    Update 2022-06-26

---
+++ pandas/autoweb001.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+simpsons = pd.read_html(
+    'https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
+
+z = len(simpsons)
+
+print(simpsons[0])
---
+++ wa/pythonwa001.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+phone_number = input('Input phone no ?')
+
+#pywhatkit.sendwhatmsg('J9dKklr2eCBGUb0Ab7xqRe',"test", 23, 20)
+pywhatkit.sendwhatmsg_to_group('J9dKklr2eCBGUb0Ab7xqRe',"test", 23, 21)
